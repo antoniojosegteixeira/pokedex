a6448e450b61fe0cbcb926fdd31b6a569a5906d5
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-04 17:36:15 -0400

    feat: add get pokemons function to hook

--- src/application/hooks/usePokemon.js
+++ src/application/hooks/usePokemon.js
@@ -1,9 +1,15 @@
 import { useContext } from "react";
-import { AppContext } from "./application/context/store";
+import { AppContext } from "../context/store";
+import { getPokemonListRequest } from "../../services/pokeapiRequests";
 
 export function usePokemon() {
   const context = useContext(AppContext);
   const { state, dispatch } = context;
 
-  return {};
+  const getPokemonList = async () => {
+    const pokemonList = await getPokemonListRequest();
+    dispatch({ type: "ADD_POKEMON_LIST", payload: pokemonList.results });
+  };
+
+  return { getPokemonList };
 }
