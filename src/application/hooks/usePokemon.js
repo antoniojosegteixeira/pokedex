eeaf3c9d4d927feddef0c631e4ead3faec7f6410
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-04 19:32:17 -0400

    feat: add filter function to usePokemon hook

--- src/application/hooks/usePokemon.js
+++ src/application/hooks/usePokemon.js
@@ -5,11 +5,16 @@ import { getPokemonListRequest } from "../../services/pokeapiRequests";
 export function usePokemon() {
   const context = useContext(AppContext);
   const { state, dispatch } = context;
+  const { pokemonList, error, filter } = state;
 
   const getPokemonList = async () => {
     const pokemonList = await getPokemonListRequest();
     dispatch({ type: "ADD_POKEMON_LIST", payload: pokemonList.results });
   };
 
-  return { getPokemonList, pokemonList: state.pokemonList };
+  const addFilter = (filter) => {
+    dispatch({ type: "ADD_POKEMON_FILTER", payload: filter });
+  };
+
+  return { pokemonList, error, filter, getPokemonList, addFilter };
 }
