c33642987f6641763757362102c4d4772d941f44
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-04 18:33:01 -0400

    feat: get pokemon list on app rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { usePokemon } from "./application/hooks/usePokemon";
 
 function App() {
+  const { pokemonList, getPokemonList } = usePokemon();
+
+  useEffect(() => {
+    if (Object.keys(pokemonList).length === 0) {
+      getPokemonList();
+    }
+  }, [getPokemonList, pokemonList]);
+
+  console.log(pokemonList);
+
   return <div>PokeApi</div>;
 }
 
