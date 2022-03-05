42c0389d0bee9f471383490ebbcf32a0f12c2aee
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-05 17:14:02 -0400

    style: type button

--- src/components/TypeButton/index.js
+++ src/components/TypeButton/index.js
@@ -4,5 +4,9 @@ import { usePokemon } from "../../application/hooks/usePokemon";
 export default function TypeButton({ type }) {
   const { filter, addFilter } = usePokemon();
 
-  return <button onClick={() => addFilter(type)}>{type}</button>;
+  return (
+    <button onClick={() => addFilter(type)} className="type-button">
+      {type}
+    </button>
+  );
 }
