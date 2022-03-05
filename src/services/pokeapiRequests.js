356c707fef187026736c86e32d6b2611688d408d
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-04 20:10:20 -0400

    feat: add single pokemon api request

--- src/services/pokeapiRequests.js
+++ src/services/pokeapiRequests.js
@@ -1,8 +1,22 @@
-import { pokemonList } from "../utils/mockResponse";
+import { pokemonList, singlePokemon } from "../utils/mockResponse";
 
 export const getPokemonListRequest = () => {
   return new Promise((resolve, reject) => {
     const response = pokemonList;
+
+    setTimeout(() => {
+      if (response) {
+        resolve(response);
+      } else {
+        reject({ message: "Error" });
+      }
+    }, 1500);
+  });
+};
+
+export const getSinglePokemonRequest = () => {
+  return new Promise((resolve, reject) => {
+    const response = singlePokemon;
 
     setTimeout(() => {
       if (response) {
