5a128609f8d03f3d4dfa4d1aaa9374fbfedee024
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-05 18:46:50 -0400

    feat: render filtered cards

--- src/components/PokemonScreen/index.js
+++ src/components/PokemonScreen/index.js
@@ -5,14 +5,39 @@ import Card from "../../components/Card";
 
 export default function PokemonScreen() {
   const { pokemonList, filter } = usePokemon();
-  return (
-    <div className="screen-grid">
-      {pokemonList.length > 0 &&
-        pokemonList.map((item) => (
+
+  /// Check if there's a filter
+  if (pokemonList && filter && pokemonList.length > 0) {
+    const filteredPokemons = pokemonList.filter((pokemon) => {
+      return pokemon.types[0].type.name === filter;
+    });
+
+    return (
+      <div className="screen-grid">
+        {filteredPokemons.map((item) => (
           <div key={item.name}>
             <Card name={item.name} id={item.id} image={item.image} />
           </div>
         ))}
-    </div>
-  );
+      </div>
+    );
+  }
+
+  /// Default pokemon screen
+  if (pokemonList && pokemonList.length > 0) {
+    return (
+      <div className="screen-grid">
+        {pokemonList.length > 0 &&
+          pokemonList.map((item) => (
+            <div key={item.name}>
+              <Card name={item.name} id={item.id} image={item.image} />
+            </div>
+          ))}
+      </div>
+    );
+  }
+
+  /// Loading
+
+  return <div></div>;
 }
