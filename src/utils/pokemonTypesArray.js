041c18a8f0e0c9a77935089953e28d2574f052cc
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-05 17:00:14 -0400

    feat: add types array file

---
+++ src/utils/pokemonTypesArray.js
@@ -0,0 +1,22 @@
+const types = [
+  "normal",
+  "fire",
+  "water",
+  "grass",
+  "flying",
+  "fighting",
+  "poison",
+  "electric",
+  "ground",
+  "rock",
+  "psychic",
+  "ice",
+  "bug",
+  "ghost",
+  "steel",
+  "dragon",
+  "dark",
+  "fairy",
+];
+
+export default types;
