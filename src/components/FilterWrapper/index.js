7f1683fe5d13ee34d7087b6448f4ae1409dd197e
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-05 21:20:28 -0400

    style: improve filter wrapper

--- src/components/FilterWrapper/index.js
+++ src/components/FilterWrapper/index.js
@@ -8,7 +8,7 @@ export default function FilterWrapper() {
   const { addFilter } = usePokemon();
 
   return (
-    <div>
+    <div className="filter-wrapper">
       {types && types.map((type) => <TypeButton type={type} key={type} />)}
       <button onClick={() => addFilter("")} className={"type-button reset"}>
         Reset
